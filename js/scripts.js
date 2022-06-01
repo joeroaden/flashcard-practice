09c2f186066a312b520bab4ed22b6540933be48c
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-01 14:34:42 -0700

    update js functionality to add pop up to buttons and remove once complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".JS-showing").toggle();
-    $(".JS-hidden").toggle();
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
+
+    $("ul#user").children("li").first().click(function() {
+        $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+    });
+
   });
-  
-  $(".clickable1").click(function() {
-    $(".operators-showing").toggle();
-    $(".operators-hidden").toggle();
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
   });
 });
