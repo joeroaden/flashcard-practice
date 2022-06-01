65e343d8889313d2b195276a0cfba825a03fe130
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-01 13:50:05 -0700

    add JS and operator definitions.  add clickable1 to toggle

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".JS-showing").toggle();
+    $(".JS-hidden").toggle();
+  });
+  
+  $(".clickable1").click(function() {
+    $(".operators-showing").toggle();
+    $(".operators-hidden").toggle();
+  });
+});
